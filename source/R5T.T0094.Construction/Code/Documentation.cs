4aa4b5cab86767ccbf956889dedf701cda192171
davidcoats <david@getsafetycone.com>
2021-11-28 22:47:37 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0094.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0094.Construction
+{
+    /// <summary>
+    /// Construction project for INamedIdentifiedFilePathed interface type definitions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0094/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0094
+{
+    /// <summary>
+    /// INamedIdentifiedFilePathed interface type definitions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
